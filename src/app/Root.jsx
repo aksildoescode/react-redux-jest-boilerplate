05418b0b1b0fb83821edb97f96786f9fcd168f6a
koceila <lounas.koceila@gmail.com>
2019-11-11 20:04:25 +0100

    add pretty-quick

--- src/app/Root.jsx
+++ src/app/Root.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 
-const App = () => <div>this file is located on app/Root.jsx </div>
+const App = () => <div>file is located on app/Root.jsx </div>
 
 const Root = ({ store }) => (
   <Provider store={store}>
